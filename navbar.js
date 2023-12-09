a41d5806b6b5fe6c6db4598afb40090a88e91246
Hannah Larsen <18hkl@queensu.ca>
2023-12-08 22:26:08 -0500

    major design and functionality changes

---
+++ navbar.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const navList = document.querySelector(".nav-list");
+  navList.classList.toggle("show");
+  /*var currentOpacity = parseFloat(window.getComputedStyle(navList).opacity);
+  navbar.style.opacity = navbar.style.opacity === "0" ? "1" : "0";*/
+}
