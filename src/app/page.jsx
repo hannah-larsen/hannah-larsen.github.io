c567b495fc2db25a192d71c97243a2d7d57efd2b
Hannah Larsen <18hkl@queensu.ca>
2026-02-22 15:20:21 -0800

    portfolio content update + dynamic resizing fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,48 +1,47 @@
 // app/page.jsx
-import { Mail, Linkedin, Instagram } from "lucide-react";
+import { Mail, Linkedin, Instagram, Youtube } from "lucide-react";
 
 export default function HomePage() {
   return (
-    <div className="mt-10 mb-10 max-w-[90%] mx-auto bg-darkg/90 rounded-3xl shadow-lg p-8 flex flex-col md:flex-row">
+    <div className="mt-10 mb-10 max-w-[90%] mx-auto bg-darkg/90 rounded-3xl shadow-lg p-6 md:p-12 lg:p-16 flex flex-col md:flex-row md:min-h-[60vh] lg:min-h-[75vh] justify-center">
       <div className="md:w-2/5 w-full flex-shrink-0 flex flex-col items-center mb-6 md:mb-0">
         <img
           src="/hannah-circle.png"
           alt="Hannah Larsen"
-          className="w-60 h-auto rounded-full p-4"
+          className="w-48 md:w-60 lg:w-72 xl:w-96 h-auto rounded-full p-4 md:p-6"
         />
         <div className="flex justify-center mt-2 space-x-4">
           <a
-            className="social-button w-14 h-14 rounded-full flex items-center justify-center"
+            className="social-button w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-20 xl:h-20 rounded-full flex items-center justify-center"
             href="mailto:hannah.larsen62@gmail.com"
           >
-            <Mail className="text-darkg w-6 h-6" />
+            <Mail className="text-darkg w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 xl:w-8 xl:h-8" />
           </a>
           <a
-            className="social-button w-14 h-14 rounded-full flex items-center justify-center"
+            className="social-button w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-20 xl:h-20 rounded-full flex items-center justify-center"
             href="https://www.linkedin.com/in/hannahlars"
             target="_blank"
           >
-            <Linkedin className="text-darkg w-6 h-6" />
+            <Linkedin className="text-darkg w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 xl:w-8 xl:h-8" />
           </a>
           <a
-            className="social-button w-14 h-14 rounded-full flex items-center justify-center"
+            className="social-button w-12 h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-20 xl:h-20 rounded-full flex items-center justify-center"
             href="https://www.instagram.com/larsn.wav"
             target="_blank"
           >
-            <Instagram className="text-darkg w-6 h-6" />
+            <Instagram className="text-darkg w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7 xl:w-8 xl:h-8" />
           </a>
         </div>
       </div>
-
-      <div className="md:w-3/5 w-full p-6 flex flex-col justify-center text-center md:text-left">
-        <h1 className="text-5xl md:text-7xl mb-2 font-mansalva text-amber-50">
+      <div className="md:w-3/5 w-full p-4 md:p-6 flex flex-col justify-center text-center md:text-left text-base md:text-lg lg:text-xl">
+        <h1 className="text-5xl md:text-7xl lg:text-8xl xl:text-9xl mb-2 font-mansalva text-amber-50">
           hannah larsen
         </h1>
-        <h2 className="text-xl md:text-2xl mb-2 font-mansalva text-amber-50">
+        <h2 className="text-lg md:text-2xl lg:text-3xl xl:text-4xl mb-2 font-mansalva text-amber-50">
           graphic designer and multimedia developer
         </h2>
         <hr className="border-t border-amber-50 my-4" />
-        <h2 className="text-xl md:text-2xl font-mansalva text-amber-50">
+        <h2 className="text-lg md:text-2xl lg:text-3xl xl:text-4xl font-mansalva text-amber-50">
           <a
             className="portfolio-link"
             href="/portfolio"
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -2,6 +2,33 @@ import ProjectCard from "@/components/ProjectCard";
 
 const projects = [
   {
+    title: "Graphic Design Showcase",
+    heading: "Misc. Personal Projects",
+    subhead: "A mix of some projects that don’t fit into the other categories that represent my personal art style.",
+    images: ["/personal2.png", "/personal3.png", "/personal1.png"],
+    description: "These personal projects were a playground for me to experiment with different styles and techniques. More of my personal work can be found on my Instagram, linked below.",
+    skills: "Procreate",
+    websiteLink: "https://www.instagram.com/larsn.wav",
+  },
+  {
+    title: "Hexcodle - Web Application",
+    heading: "Personal Project",
+    subhead: "Hexcodle is a color-guessing game inspired by Wordle, where players attempt to guess a hex code in just a few tries. The goal was to create an engaging, interactive web app that blends design knowledge with puzzle-solving.",
+    images: ["/hexcodle2.png", "/hexcodle1.png"],
+    description: "This project brought together several of my creative strengths: UI design, web development, illustration, blog copywriting and audio engineering. I’m especially proud of how we elevated a simple concept with personalized touches like pixel art and sound design. Hexcodle averages around 1000 daily active users, with a lifetime of approximately 250,000 users.",
+    skills: "Figma • JavaScript (React.js)",
+    websiteLink: "https://www.hexcodle.com",
+  },
+  {
+    title: "Chromara Card Illustrations",
+    heading: "Chromara Virtual TCG",
+    subhead: "Working closely alongside the project manager & co-artist, I illustrated various virtual cards. I worked off prompts and visual references to create funky fictional characters in Procreate.",
+    images: ["/chromara2.png", "/chromara1.png", "/chromara3.png", "/chromara4.png", "/chromara5.png"],
+    description: "My first big illustration project. I learned how to adapt my artistic style to match the game's unique watercolour aesthetic. I collaborated with team members on daily feedback loops to ensure each final illustration matches the creative vision.",
+    skills: "Procreate",
+    websiteLink: "https://instagram.com/chromara_tcg",
+  },
+  {
     title: "Motorsport Bundle Launch",
     heading: "Element Fire Extinguishers",
     subhead: "To promote Element’s new E100 roll bar mount, I helped launch the Motorsport Bundle tailored for racing enthusiasts. The bundle combined the new E100 mount with an E100 fire extinguisher and a custom Motorsport Decal Sheet.",
@@ -11,31 +38,13 @@ const projects = [
     websiteLink: "https://elementfire.com/blogs/articles/introducing-the-motorsport-bundle",
   },
   {
-    title: "Element Social Media Posts",
+    title: "Element Social Media Graphics",
     heading: "Element Fire Extinguishers",
     subhead: "I designed social media graphics for Element with a focus on a minimalist, Euro-inspired aesthetic. The content ranged from weekly blog promotions to brand collaborations and shopping events like holiday sales. Each post needed to feel clean and on-brand while still standing out in a crowded feed.",
     images: ["/stelvio-social1.png", "/stelvio-social2.png", "/stelvio-social4.png", "/stelvio-social3.png"],
     description: "This taught me how to design for a wide range of content types while maintaining a cohesive brand look. I also sharpened my technical skills in Photoshop, especially when it came to styling type, working with mockups, and creating templates for faster turnaround.",
     skills: "Photoshop • Shopify",
     websiteLink: "",
-  },
-  {
-    title: "Hexcodle",
-    heading: "Personal Project — Web Application",
-    subhead: "Hexcodle is a color-guessing game inspired by Wordle, where players attempt to guess a hex code in just a few tries. The goal was to create an engaging, interactive web app that blends design knowledge with puzzle-solving.",
-    images: ["/hexcodle2.png", "/hexcodle1.png", "/hexcodle3.png"],
-    description: "This project brought together several of my creative strengths: design, web development, illustration, and audio. I’m especially proud of how we elevated a simple concept with personalized touches like pixel art and sound design. It was a valuable exercise in UI development and cross-disciplinary creativity.",
-    skills: "Figma • JavaScript (React.js)",
-    websiteLink: "https://www.hexcodle.com",
-  },
-  {
-    title: "Triple Siege TCG Illustrations",
-    heading: "Triple Siege Trading Card Game",
-    subhead: "Working closely alongside the project manager & co-artist, I illustrated various virtual cards. I worked off prompts and visual references to create funky fictional characters in Procreate. This project is ongoing!",
-    images: ["/triple-2.png", "/triple-1.png", "/triple-3.png"],
-    description: "My first big illustration project. I learned how to adapt my artistic style to match the game's aesthetic. I collaborated with team members on daily feedback loops to ensure each final illustration matches the creative vision.",
-    skills: "Procreate",
-    websiteLink: "https://triple-siege.vercel.app/",
   },
   {
     title: "COMPSA Circuit Branding",
@@ -55,24 +64,16 @@ const projects = [
     skills: "Canva • Figma",
     websiteLink: "",
   },
-   {
-    title: "Apartment Coffee Shop Vlog",
-    heading: "Personal Project - Youtube Video",
-    subhead: "This was a personal video project where I documented the process of transforming my apartment into a pop-up coffee shop for a day. I had hosted a few of these events before, but this was the first time I filmed the full setup and experience, both as a way to share it with friends and to explore a new creative medium.",
-    images: ["/youtube1.png"],
-    description: "This project was a great opportunity to explore content creation and video editing from start to finish. I learned about camera setup, gathering effective b-roll, and structuring clips into a cohesive story. It also gave me insight into the YouTube upload process, including thumbnails, audio levels, and pacing for an online audience.",
-    skills: "CapCut • Photoshop • Ableton Live",
-    websiteLink: "https://www.youtube.com/watch?v=TDPZfCnWCEw",
-  },
   {
     title: "Audio Project Showcase",
-    heading: "Mini Portfolio - Personal & Client Projects",
+    heading: "Personal & Client Projects",
     subhead: "Though my main work lives in the visual world, sound is where I experiment, play, and push boundaries. Step into my sonic playground and explore how I build mood and meaning through audio.",
     images: ["audio-image.png"],
     description: "From crafting custom scores for games to generative music built with code, these projects blend tech, texture, and tone. It’s a space where field recordings meet synths, and storytelling happens through sound.",
     skills: "Ableton Live • Reaper • Audacity",
     websiteLink: "/audio",
   },
+   
 ];
 
 export default function PortfolioPage() {
